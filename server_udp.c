3acc400164a7988c0d26f2cd9cc504e8ef331320
Adrian <misiakadrian@onet.eu>
2019-04-17 15:41:12 +0200

    removing disconnecting users

--- server_udp.c
+++ server_udp.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 
 #define ST_PORT 61234
-#define MAX_CONNECTIONS 8
+#define MAX_CONNECTIONS 3
 #define BUF_LEN 80
 
 DWORD WINAPI listener_t(void *params);
@@ -12,6 +12,7 @@ unsigned int curr_connections = 0;
 
 HANDLE *threads_c;
 struct sockaddr_in users[MAX_CONNECTIONS];
+int socket_free[MAX_CONNECTIONS];
 
 int exit_program;
 
@@ -24,6 +25,8 @@ int main() {
 	s = socket(AF_INET, SOCK_DGRAM, 17);
 	curr_connections = 0;
 	exit_program = 0;
+	for(int i=0; i<MAX_CONNECTIONS; i++)
+		socket_free[i] = 1;
 	
 	struct sockaddr_in sa;
 	memset((void *)(&sa), 0, sizeof(sa));
@@ -80,19 +83,42 @@ DWORD WINAPI listener_t(void *params) {
 		memset(buf, '\0', BUF_LEN);
 		if (recvfrom(s, buf, BUF_LEN, 0, (struct sockaddr *) &from, &fromlen) > 0) {
 			if (strcmp("JOIN", buf) == 0 && curr_connections < MAX_CONNECTIONS) {
-				// Remember user
-				memcpy(&users[curr_connections], &from, sizeof(struct sockaddr_in));
-				curr_connections++;
-				printf("New user connected\n");
+				for(int i=0; i<MAX_CONNECTIONS; i++){
+					if(socket_free[i]){
+						// Remember user
+						memcpy(&users[i], &from, sizeof(struct sockaddr_in));
+						curr_connections++;
+						socket_free[i] = 0;
+						printf("New user connected\n");
+						break;
+					}
+				}
 			}
-			else if (strcmp("KONIEC", buf) == 0) {
+			else if (strcmp("KONIEC", buf+5) == 0) {
 				// Search for user in user array and remove him.
+				curr_connections -= 1;
+				for(int index=0; index<MAX_CONNECTIONS; index++){
+					if(memcmp(&users[index], &from, sizeof(struct sockaddr_in)) == 0){
+						socket_free[index] = 1;
+						printf("User disconnected\n");
+						break;
+					}
+				}
 			}
+			int is_user = 0;
 			// Resend received message to everyone
+			for(int i=0; i<MAX_CONNECTIONS; i++){
+				if(memcmp(&users[i], &from, sizeof(struct sockaddr_in)) == 0){
+				is_user = 1;
+				break;
+				}
+			}
+			if(!is_user)
+				continue;
 			printf("%s\n", buf);
-			for (int i = 0; i < curr_connections; i++) {
-				sendto(s, buf, 80, 0, (struct sockaddr*)&(users[i]),
-					sizeof(struct sockaddr_in));
+			for (int i = 0; i < MAX_CONNECTIONS; i++) {
+				if(socket_free[i] == 0)
+					sendto(s, buf, 80, 0, (struct sockaddr*)&(users[i]), sizeof(struct sockaddr_in));
 			}
 		}
 	}
